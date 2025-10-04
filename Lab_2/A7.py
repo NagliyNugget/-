df7c157f168620a6c14bc8d3c6f754305a898ffd
Игнатий Грико <90447281+NagliyNugget@users.noreply.github.com>
2025-10-04 10:14:57 +0300

    добавил определение цвета клеток

--- Lab_2/A7.py
+++ Lab_2/A7.py
@@ -2,3 +2,6 @@ x1 = int(input("Введите x1: "))
 y1 = int(input("Введите y1: "))
 x2 = int(input("Введите x2: "))
 y2 = int(input("Введите y2: "))
+
+color1 = 'white' if (x1+y1)%2==0 else 'black'
+color2 = 'white' if (x2+y2)%2==0 else 'black'
