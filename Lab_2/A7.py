bb0ba4198a3c5830f8c9bf87e2da6a875fce96a8
Игнатий Грико <90447281+NagliyNugget@users.noreply.github.com>
2025-10-04 10:17:51 +0300

    дорешал A7

--- Lab_2/A7.py
+++ Lab_2/A7.py
@@ -5,3 +5,6 @@ y2 = int(input("Введите y2: "))
 
 color1 = 'white' if (x1+y1)%2==0 else 'black'
 color2 = 'white' if (x2+y2)%2==0 else 'black'
+
+if color1==color2: print('Yes', color1)
+else: print('No')
