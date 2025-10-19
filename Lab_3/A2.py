98c950830f52cab60eb07ba3c5751f859b42e481
Игнатий Грико <90447281+NagliyNugget@users.noreply.github.com>
2025-10-19 17:51:00 +0300

    Добавил проверку пароля на наличие специальных и непредусмотренных символов

--- Lab_3/A2.py
+++ Lab_3/A2.py
@@ -8,13 +8,19 @@ if len(password) != 8:
     errors = errors + 'Длина пароля не равна 8 \n'
     isValid = False
 if password.lower() == password:
-    errors = errors + 'Отсутствуют заглавные буквы \n'
+    errors = errors + 'В пароле отсутствуют заглавные буквы \n'
     isValid = False
 if password.upper() == password:
-    errors = errors + 'Отсутствуют строчные буквы \n'
+    errors = errors + 'В пароле отсутствуют строчные буквы \n'
     isValid = False
-if not any(map(str.isdigit, password)):
-    errors = errors + 'Отсутствуют цифры \n'
+if not any(symbol.isdigit() for symbol in password):
+    errors = errors + 'В пароле отсутствуют цифры \n'
+    isValid = False
+if ('*' not in password) and ('-' not in password) and ('#' not in password):
+    errors = errors + 'В пароле отсутствуют специальные символы \n'
+    isValid = False
+if (set(password) - set(allowed_sym)) != set():
+    errors = errors + 'В пароле используются непредусмотренные символы \n'
     isValid = False
 if isValid == True:
     print("Надежный пароль")
