7b5f7958f9283339860aea7231fc7aae8a31cd80
Игнатий Грико <90447281+NagliyNugget@users.noreply.github.com>
2025-10-20 23:31:43 +0300

    Добавил расчёт суммы оплата в зависимости от объёма воды

--- Lab_3/A3.py
+++ Lab_3/A3.py
@@ -5,3 +5,12 @@ if current >= previous:
     volume = current - previous
 else:
     volume = 10000 - previous + current
+    
+if volume <= 300:
+    payment = 21.0
+elif volume <= 600:
+    payment = 21 + (volume - 300) * 0.06
+elif volume <= 800:
+    payment = 21 + 300 * 0.06 + (volume - 600) * 0.04
+else:
+    payment = 21 + 300 * 0.06 + 200 * 0.04 + (volume - 800) * 0.025
