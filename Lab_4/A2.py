36836b86f486aca33d7d5fa63370aabf70c10450
Игнатий Грико <90447281+NagliyNugget@users.noreply.github.com>
2025-10-24 09:12:53 +0300

    Добавил вывод правого треугольника и рамки

--- Lab_4/A2.py
+++ Lab_4/A2.py
@@ -6,3 +6,18 @@ for i in range(n):
     for j in range(m):
         print('#', end='')
     print()
+
+print(f"ПРАВЫЙ ТРЕУГОЛЬНИК:")
+for i in range(n):
+    for j in range(i+1):
+        print('#', end='')
+    print()
+
+print(f"РАМКА {n}*{m}:")
+for i in range(n):
+    for j in range(m):
+        if i == 0 or i == n - 1 or j == 0 or j == m - 1:
+            print("#", end='')
+        else:
+            print(" ", end='')
+    print()
