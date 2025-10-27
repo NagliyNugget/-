a4ec904b3227e59e6c50db6301c002e3b8c03276
Игнатий Грико <90447281+NagliyNugget@users.noreply.github.com>
2025-10-27 20:04:46 +0300

    Добавил нахождение самой длинной последовательности потерянных пакетов

--- Lab_4/A3.py
+++ Lab_4/A3.py
@@ -10,5 +10,16 @@ total_packets = len(packets)
 
 lost_packets = packets.count('0')
 
+max_streak = 0
+streak = 0
+
+for packet in packets:
+    if packet == '0':
+        streak += 1
+        max_streak = max(max_streak, streak)
+    else:
+        streak = 0
+
 print(f"Общее число пакетов: {total_packets}")
 print(f"Число потерянных пакетов: {lost_packets}")
+print(f"Длина самой длинной последовательности потерянных пакетов: {max_streak}")
